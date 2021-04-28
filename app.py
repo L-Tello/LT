14bd4729dfa247f4a09c5654b4e577d4b71c6f23
L-Tello <81892001+L-Tello@users.noreply.github.com>
2021-04-27 21:24:33 -0600

    prueba

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 def principal():
     return {"nombre":"Ludwin Tello", "carnet":"202010303"}
 
-
+a
 if __name__ == '__main__':
     puerto = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0',port=puerto)
